00224b13d950c15354f2731e037f6e730e31a6de
Adrià Lorente <adria810@gmail.com>
2025-07-22 21:42:03 +0200

    feat(model): create Persona class with nombre, edad and email fields

---
+++ src/main/java/com/alaw810/model/Person.java
@@ -0,0 +1,40 @@
+package com.alaw810.model;
+
+import com.alaw810.annotations.JsonSerializable;
+
+@JsonSerializable(directory = "data/output")
+public class Person {
+    private String name;
+    private int age;
+    private String email;
+
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
